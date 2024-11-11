3ad4e75a00a4ba6ad033935e005a395fefb29cb9
Realitylion <dhanushbaalaji123@gmail.com>
2024-11-11 19:53:10 +0530

    production ready

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const productRoutes = require('./routes/product');
 const categoryRoutes = require('./routes/category');
